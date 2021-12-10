4f02f596db3cffded99aa517cd6f978e766d8314
davidcsussex <david.clarke@escg.ac.uk>
2021-12-10 07:35:27 +0000

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float nextStateTimer;
+    private int state;
+    private string stateText;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int state = 0;
+        nextStateTimer = 2;
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        ProcessStates();
+
+    }
+
+
+
+    // State logic - switch states depending on what logic we want to apply
+    void ProcessStates()
+    {
+        nextStateTimer -= Time.deltaTime;
+
+        if( state == 0 )
+        {
+            Idle();
+
+            if( nextStateTimer < 0 )
+            {
+                state = 1;
+                nextStateTimer = 2;
+            }
+        }
+
+        if( state == 1 )
+        {
+
+            Turn();
+            if( nextStateTimer < 0 )
+            {
+                state = 2;
+                nextStateTimer = 10;
+            }
+
+        }
+
+        if( state == 2 )
+        {
+            if( nextStateTimer < 0 )
+            {
+                state = 0;
+                nextStateTimer = 5;
+            }
+
+            Walk();
+        }
+
+    }
+
+
+    // Different AI Update methods
+    void Idle()
+    {
+        print("Idle");
+        stateText = "Idle";
+        anim.SetBool("walk", false);
+    }
+
+    void Turn()
+    {
+        print("Turn");
+        stateText = "Turn";
+    }
+
+    void Walk()
+    {
+        anim.SetBool("walk", true);
+        print("Walk");
+        stateText = "Walk";
+    }
+
+
+
+
+
+    void OnGUI ()
+    {
+        string content = nextStateTimer.ToString();
+        string state = stateText;
+        GUILayout.Label($"<color='black'><size=40>State= {state}\n{content}</size></color>");
+    }
+}
+
